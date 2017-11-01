eb51bdd38c54eccae373654c286812abb84c1a33
pan7an <pan7an@users.noreply.github.com>
2017-11-01 13:16:53 +0800

    add selenium

--- service/data/paper/paper.py
+++ service/data/paper/paper.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 from tornado import gen
+from selenium import webdriver
 
 import conf.path as path
 from service.data.base import DataService
@@ -49,3 +50,20 @@ class PaperDataService(DataService):
                                proxy_port=port)
 
         raise gen.Return(ret)
+
+    @gen.coroutine
+    def refresh_article(self, id):
+        """
+        利用selenium刷新网页
+        :param id:
+        :return:
+        """
+
+        host, port = yield self.get_ip_proxy()
+        proxy = "{}:{}".format(host,port)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--proxy-server=http://%s' % proxy)
+        chrome = webdriver.Chrome(chrome_options=chrome_options)
+        chrome.get(path.PAPER_ARTICLE.format(id))
+
+        raise gen.Return(True)
