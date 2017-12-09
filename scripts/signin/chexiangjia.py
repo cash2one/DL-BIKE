6fc19dc70c44dc8afd8bbb03c5e7c17d71cc0d94
pan7an <pan7an@users.noreply.github.com>
2017-12-09 14:19:18 +0800

    调整车享家

--- scripts/signin/chexiangjia.py
+++ scripts/signin/chexiangjia.py
@@ -32,31 +32,15 @@ class Chexiangjia(Parser):
         """
 
         routine_header = ObjectDict({
-            "17621856713": ObjectDict({
-                'Host': 'cxj.activity.chexiang.com',
-                'Referer': 'https://cxj.activity.chexiang.com/cx/cxj/cxjappweb/welfare/index.shtml',
-                'X-Requested-With': 'XMLHttpRequest',
-                'Accept-Encoding': 'br, gzip, deflate',
-                'Accept-Language': 'zh-CN',
-                'Content-Type': 'application/json;charset=UTF-8',
-                'cookie': 'user_trace_cookie=CiCcIloRcyFtlXYEZEWIAg==; wemall_equid_c=723D3C53-952C-4FF2-8C80-EE0D47788F4B; wemall_openid_c=""; wemall_opentype_c=6; wemall_userkey_c=62882c7f-3644-46d1-ad1c-c8131d339161; analysis_sign=10652305; wemall_cxid_c=10652305; _ga=GA1.2.1080391224.1509372664; wcid=26D47ADB-80AD-4FAF-BD19-06952B0647E7',
-                'User-Agent': '{  "clientId" : "93c36ab85c994b04b184c3653297eaed",  "deviceId" : "723D3C53-952C-4FF2-8C80-EE0D47788F4B",  "appCode" : "MongoToC",  "plateformType" : "ios",  "signature" : "C5A6EF5B83B374489C7F8BD23C59FB68",  "origin" : "Mozilla\/5.0 (iPhone; CPU iPhone OS 11_1_2 like Mac OS X) AppleWebKit\/604.3.5 (KHTML, like Gecko) Mobile\/15B202",  "userToken" : "MTAwMDU5ZjczMmY1NWE2ZGQ5ZjUxMDY1MjMwNZPa1JVvyV9odOSfXbwvM6z9jGrb",  "sourceCode" : "APPStore",  "deviceManufacturer" : "iPhone",  "appVersion" : "4.4",  "userAccount" : "17621856713",  "signVersion" : "0.2"}',
-
-            }),
-            "18390536344": ObjectDict({
-                'Host': 'cxj.activity.chexiang.com',
-                'Referer': 'https://cxj.activity.chexiang.com/cx/cxj/cxjappweb/welfare/index.shtml',
-                'X-Requested-With': 'XMLHttpRequest',
-                'Accept-Encoding': 'br, gzip, deflate',
-                'Accept-Language': 'zh-CN',
-                'Content-Type': 'application/json;charset=UTF-8',
-                'cookie': 'user_trace_cookie=CiCcIloRcyFtlXYEZEWIAg==; wemall_equid_c=723D3C53-952C-4FF2-8C80-EE0D47788F4B; wemall_openid_c=""; wemall_opentype_c=6; wemall_userkey_c=62882c7f-3644-46d1-ad1c-c8131d339161; analysis_sign=10652305; wemall_cxid_c=10652305; _ga=GA1.2.1080391224.1509372664; wcid=26D47ADB-80AD-4FAF-BD19-06952B0647E7',
-                'User-Agent': '{  "clientId" : "93c36ab85c994b04b184c3653297eaed",  "deviceId" : "723D3C53-952C-4FF2-8C80-EE0D47788F4B",  "appCode" : "MongoToC",  "plateformType" : "ios",  "signature" : "C5A6EF5B83B374489C7F8BD23C59FB68",  "origin" : "Mozilla\/5.0 (iPhone; CPU iPhone OS 11_1_2 like Mac OS X) AppleWebKit\/604.3.5 (KHTML, like Gecko) Mobile\/15B202",  "userToken" : "MTAwMDU5ZjczMmY1NWE2ZGQ5ZjUxMDY1MjMwNZPa1JVvyV9odOSfXbwvM6z9jGrb",  "sourceCode" : "APPStore",  "deviceManufacturer" : "iPhone",  "appVersion" : "4.4",  "userAccount" : "17621856713",  "signVersion" : "0.2"}',
-            })
-        })
-
-        routine_header = ObjectDict({
             # html 页面 header
+            'Host': 'cxj.activity.chexiang.com',
+            'Referer': 'https://cxj.activity.chexiang.com/cx/cxj/cxjappweb/welfare/index.shtml',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Accept-Encoding': 'br, gzip, deflate',
+            'Accept-Language': 'zh-CN',
+            'Content-Type': 'application/json;charset=UTF-8',
+            'cookie': 'user_trace_cookie=CiCcIloRcyFtlXYEZEWIAg==; wemall_equid_c=723D3C53-952C-4FF2-8C80-EE0D47788F4B; wemall_openid_c=""; wemall_opentype_c=6; wemall_userkey_c=62882c7f-3644-46d1-ad1c-c8131d339161; analysis_sign=10652305; wemall_cxid_c=10652305; _ga=GA1.2.1080391224.1509372664; wcid=26D47ADB-80AD-4FAF-BD19-06952B0647E7',
+            'User-Agent': '{  "clientId" : "93c36ab85c994b04b184c3653297eaed",  "deviceId" : "723D3C53-952C-4FF2-8C80-EE0D47788F4B",  "appCode" : "MongoToC",  "plateformType" : "ios",  "signature" : "C5A6EF5B83B374489C7F8BD23C59FB68",  "origin" : "Mozilla\/5.0 (iPhone; CPU iPhone OS 11_1_2 like Mac OS X) AppleWebKit\/604.3.5 (KHTML, like Gecko) Mobile\/15B202",  "userToken" : "MTAwMDU5ZjczMmY1NWE2ZGQ5ZjUxMDY1MjMwNZPa1JVvyV9odOSfXbwvM6z9jGrb",  "sourceCode" : "APPStore",  "deviceManufacturer" : "iPhone",  "appVersion" : "4.4",  "userAccount" : "17621856713",  "signVersion" : "0.2"}',
         })
 
         jdata = ObjectDict({
@@ -65,8 +49,7 @@ class Chexiangjia(Parser):
             'latitude': '31.134585',
         })
 
-        res = yield http_post("https://cxj.activity.chexiang.com/service/dailyfuli/list/0", jdata,
-                              headers=routine_header)
+        res = yield http_post("https://cxj.activity.chexiang.com/service/dailyfuli/list/0", jdata, headers=routine_header)
         for store in res.obj.dailyFuliVos:
             yield self.get_dailyredpacket(store['prizeCode'])
 
@@ -90,8 +73,7 @@ class Chexiangjia(Parser):
             'User-Agent': '{  "clientId" : "93c36ab85c994b04b184c3653297eaed",  "deviceId" : "723D3C53-952C-4FF2-8C80-EE0D47788F4B",  "appCode" : "MongoToC",  "plateformType" : "ios",  "signature" : "C5A6EF5B83B374489C7F8BD23C59FB68",  "origin" : "Mozilla\/5.0 (iPhone; CPU iPhone OS 11_1_2 like Mac OS X) AppleWebKit\/604.3.5 (KHTML, like Gecko) Mobile\/15B202",  "userToken" : "MTAwMDU5ZjczMmY1NWE2ZGQ5ZjUxMDY1MjMwNZPa1JVvyV9odOSfXbwvM6z9jGrb",  "sourceCode" : "APPStore",  "deviceManufacturer" : "iPhone",  "appVersion" : "4.4",  "userAccount" : "17621856713",  "signVersion" : "0.2"}',
         })
 
-        yield http_get("https://cxj.activity.chexiang.com/service/dailyfuli/receive?prizeCode={}".format(prize_code),
-                       headers=header)
+        yield http_get("https://cxj.activity.chexiang.com/service/dailyfuli/receive?prizeCode={}".format(prize_code), headers=header)
 
     @gen.coroutine
     def get_redpacket(self, item_id, store_id):
@@ -112,9 +94,7 @@ class Chexiangjia(Parser):
             'User-Agent': '{  "clientId" : "93c36ab85c994b04b184c3653297eaed",  "deviceId" : "723D3C53-952C-4FF2-8C80-EE0D47788F4B",  "appCode" : "MongoToC",  "plateformType" : "ios",  "signature" : "C5A6EF5B83B374489C7F8BD23C59FB68",  "origin" : "Mozilla\/5.0 (iPhone; CPU iPhone OS 11_1_2 like Mac OS X) AppleWebKit\/604.3.5 (KHTML, like Gecko) Mobile\/15B202",  "userToken" : "MTAwMDU5ZjczMmY1NWE2ZGQ5ZjUxMDY1MjMwNZPa1JVvyV9odOSfXbwvM6z9jGrb",  "sourceCode" : "APPStore",  "deviceManufacturer" : "iPhone",  "appVersion" : "4.4",  "userAccount" : "17621856713",  "signVersion" : "0.2"}'
         })
 
-        res = yield http_get(
-            "https://cxj.activity.chexiang.com/service/fuliv2/luck?itemId={0}&storeId={1}".format(item_id, store_id),
-            headers=header)
+        res = yield http_get("https://cxj.activity.chexiang.com/service/fuliv2/luck?itemId={0}&storeId={1}".format(item_id, store_id), headers=header)
 
     @gen.coroutine
     def get_jufengyuan(self):
@@ -135,9 +115,7 @@ class Chexiangjia(Parser):
             'User-Agent': '{  "clientId" : "93c36ab85c994b04b184c3653297eaed",  "deviceId" : "723D3C53-952C-4FF2-8C80-EE0D47788F4B",  "appCode" : "MongoToC",  "plateformType" : "ios",  "signature" : "C5A6EF5B83B374489C7F8BD23C59FB68",  "origin" : "Mozilla\/5.0 (iPhone; CPU iPhone OS 11_1_2 like Mac OS X) AppleWebKit\/604.3.5 (KHTML, like Gecko) Mobile\/15B202",  "userToken" : "MTAwMDU5ZjczMmY1NWE2ZGQ5ZjUxMDY1MjMwNZPa1JVvyV9odOSfXbwvM6z9jGrb",  "sourceCode" : "APPStore",  "deviceManufacturer" : "iPhone",  "appVersion" : "4.4",  "userAccount" : "17621856713",  "signVersion" : "0.2"}',
         })
 
-        res = yield http_get(
-            "https://cxj.activity.chexiang.com/service/fuliv2/getStoreSchemeItem?storeId=&longitude=121.373299&latitude=31.312585&cityName=%E4%B8%8A%E6%B5%B7%E5%B8%82",
-            headers=routine_header)
+        res = yield http_get("https://cxj.activity.chexiang.com/service/fuliv2/getStoreSchemeItem?storeId=&longitude=121.373299&latitude=31.312585&cityName=%E4%B8%8A%E6%B5%B7%E5%B8%82", headers=routine_header)
         for item in res.obj.get('items'):
             yield self.get_redpacket(item['sid'], res.obj.store.storeId)
 
@@ -146,7 +124,7 @@ class Chexiangjia(Parser):
         try:
             self.logger.debug("[chexiangjia]start in:{}".format(curr_now()))
             yield self.get_routine()
-            # yield self.get_jufengyuan()
+            yield self.get_jufengyuan()
         except Exception as e:
             self.logger.error(traceback.format_exc())
         finally:
